be8dd45efb5203634d3dda994d368728c2923f53
Ben <bmurray@softwareinstitute.com>
2021-10-19 17:16:32 +0100

    Final without Cucumber

--- src/test/java/com/softwareinstitute/training/murray/ben/cucumber/IntCheck.java
+++
@@ -1,7 +0,0 @@
-package com.softwareinstitute.training.murray.ben.cucumber;
-
-public class IntCheck {
-    static String check(int expected, int actual) {
-        return expected == actual ? "Yes" : "No";
-    }
-}
--- src/test/java/com/softwareinstitute/training/murray/ben/cucumber/StringCheck.java
+++
@@ -1,7 +0,0 @@
-package com.softwareinstitute.training.murray.ben.cucumber;
-
-public class StringCheck {
-    static String check(String expected, String actual) {
-        return expected.equals(actual) ? "Yes" : "No";
-    }
-}
