ae71debe9e94468a1d588202ef809a71b7a80789
Ben <bmurray@softwareinstitute.com>
2021-10-19 17:11:57 +0100

    Removed Fantasy Book junit tests to see if coverage is the same

--- src/test/java/com/softwareinstitute/training/murray/ben/FantasyBookTest.java
+++ src/test/java/com/softwareinstitute/training/murray/ben/FantasyBookTest.java
@@ -10,31 +10,6 @@ public class FantasyBookTest {
     FantasyBook myFantasyBook = new FantasyBook("Two Towers", "Tolkien", "Paperback", 352);
 
     @Test
-    public void  testFantasyBookItemTitleMethod() {
-        assertEquals("This fantasy book is not Two Towers.", "Two Towers", myFantasyBook.getItemTitle());
-    }
-
-    @Test
-    public void  testFantasyBookWriterNameMethod() {
-        assertEquals("This fantasy book is not by Tolkien.", "Tolkien", myFantasyBook.getWriterName());
-    }
-
-    @Test
-    public void  testFantasyBookCoverTypeMethod() {
-        assertEquals("This fantasy book is not a paperback.", "Paperback", myFantasyBook.getCoverType());
-    }
-
-    @Test
-    public void  testFantasyBookPagesMethod() {
-        assertEquals("This fantasy book is not 352 pages.", 352, myFantasyBook.getPages());
-    }
-
-    @Test
-    public void testFictional() {
-        assertEquals("Wrong output.", "Tells a fictional story.", myFantasyBook.fictionStory());
-    }
-
-    @Test
     public void testTestToString() {
         assertEquals("Wrong output.", "Class: Book-Fantasy\nItem title: Two Towers\nWriter: Tolkien\nCoverType: Paperback\nNumber of pages: 352\nTells a fictional story.", myFantasyBook.toString());
     }
